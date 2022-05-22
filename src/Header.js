2142187a0ae34e5da3df6b821fb4443036602618
yequan99 <60564388+yequan99@users.noreply.github.com>
2022-05-22 21:29:22 +0800

    Delete src directory

--- src/Header.js
+++
@@ -1,9 +0,0 @@
-function Header(){
-    return (
-        <h1 style={{textAlign: "center"}}>
-            HANGMAN GAME
-        </h1>
-    )
-}
-
-export default Header;
--- src/RandomWords.js
+++
@@ -1,7 +0,0 @@
-let words = ["HELLO", "WORLD", "TIKTOK", "YOUTH", "CAMP"]
-
-function RandomWords(){
-    return words[Math.floor(Math.random() * words.length)];
-}
-
-export { RandomWords };
